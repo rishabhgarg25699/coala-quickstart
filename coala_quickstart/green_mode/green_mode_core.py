8bbd4dab13853e61a1cd1a2634d0c6da72b09716
Ishan Srivastava <ishan.srt@gmail.com>
2018-08-13 19:31:00 +0530

    green_mode_core.py: Fix a bug
    os.join --> os.path.join()

--- coala_quickstart/green_mode/green_mode_core.py
+++ coala_quickstart/green_mode/green_mode_core.py
@@ -52,7 +52,7 @@ def green_mode(project_dir: str, ignore_globs, bears, bear_settings_obj,
     """
     from coala_quickstart.green_mode.filename_operations import (
         check_filename_prefix_postfix)
-    ignore_globs.append(os.join(project_dir, '.git', '**'))
+    ignore_globs.append(os.path.join(project_dir, '.git', '**'))
     project_data = project_dir + os.sep + PROJECT_DATA
 
     # Currently as a temporary measure, recreating the file at each run from
